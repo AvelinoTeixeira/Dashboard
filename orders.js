584e679d48a01de980022a06bd5bb0609e22ac59
AvelinoTeixeira <129909440+AvelinoTeixeira@users.noreply.github.com>
2023-07-14 07:50:57 +0100

    Initial commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        shipping:'Pedding',
+    },
+    {
+        productName:'LARVENDER KF102 Drone',
+        productNumber:'36378',
+        paymentStatus:'Refuned',
+        shipping:'Declined',
+    },
+    {
+        productName:'Ruko F11 Pro Drone',
+        productNumber:'49347',
+        paymentStatus:'Due',
+        shipping:'Pending',
+    },
+    {
+        productName:'Drone with Camera Drone',
+        productNumber:'969696',
+        paymentStatus:'Paid',
+        shipping:'Delivered',
+    },
+    {
+        productName:'GPS 4k Drone',
+        productNumber:'22821',
+        paymentStatus:'Paid',
+        shipping:'Delivered',
+    },
+    {
+        productName:'DJI Air',
+        productNumber:'81475',
+        paymentStatus:'Due',
+        shipping:'Pending',
+    },
+    {
+        productName:'Lorenge Drone',
+        productNumber:'00482',
+        paymentStatus:'Paid',
+        shipping:'Delivered',
+    }
+]
